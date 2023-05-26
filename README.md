d8758daac209e612f405c8117d6b450de703aa25
Yuguanhai <76604035+AlphaYuguanhai@users.noreply.github.com>
2023-05-26 18:09:02 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# LeNet-5-PyTorch-MNIST-noob
-LeNet-5，可以在MNIST数据集上训练和测试
+# PyTorch实现LeNet（非常基础）
+
+使用此仓库，可以初步了解PyTorch的用法，也可以自己试着去训练模型。适合入门用户使用。
+
+作者的b站([榆关海的个人空间_哔哩哔哩_bilibili](https://space.bilibili.com/51144635?spm_id_from=333.1007.0.0))
+
+网络结构代码是自己照着教程写的
+
+训练代码是用https://github.com/ChawDoe/LeNet5-MNIST-PyTorch改的
+
+测试代码是自己写的
+
+## 训练
+
+运行train.py，默认训练100轮。每轮都测试。
+
+## 测试
+
+运行test.py，加载模型并且测试。
+
+会输出一个batch的预测结果。
+
+## 第三方库需求
+PyTorch、torchvision（任意版本）
+
+
+
+
+
---
+++ train.py
@@ -0,0 +1,78 @@
+import numpy as np
+import os
+import torch
+from torchvision.datasets.mnist import MNIST
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+from LeNet import LeNet
+import matplotlib.pyplot as plt
+import os
+
+def plot(total_acc):
+    fig = plt.figure()
+    plt.plot(total_acc, label='Accuracy')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('准确率变化图')
+    plt.show()
+    fig.savefig('./准确率变化图.png')
+
+if __name__ == '__main__':
+    os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    batch_size = 256
+    train_dataset = MNIST(root='./train', train=True, transform=ToTensor())
+    test_dataset = MNIST(root='./test', train=False, transform=ToTensor())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size,shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    model = LeNet().to(device)
+    sgd = SGD(model.parameters(), lr=1e-1)
+    loss_fn = CrossEntropyLoss()
+    all_epoch = 50
+    prev_acc = 0
+    print("=========================开始训练=========================")
+    for current_epoch in range(all_epoch):
+        model.train()
+        for idx, (train_x, train_label) in enumerate(train_loader):
+            if(idx%100==99):
+                print("第{}轮-进度{}/50000".format(current_epoch,(idx+1)*batch_size))
+            train_x = train_x.to(device)
+            train_label = train_label.to(device)
+            sgd.zero_grad()
+            predict_y = model(train_x.float())
+            loss = loss_fn(predict_y, train_label.long())
+            loss.backward()
+            sgd.step()
+        print("第{}轮训练完成，开始测试".format(current_epoch))
+        all_correct_num = 0
+        all_sample_num = 0
+        model.eval()
+        total_epoch = []
+        total_acc = []
+        for idx, (test_x, test_label) in enumerate(test_loader):
+            test_x = test_x.to(device)
+            test_label = test_label.to(device)
+            predict_y = model(test_x.float()).detach()
+            predict_y =torch.argmax(predict_y, dim=-1)
+            current_correct_num = predict_y == test_label
+            # print(current_correct_num.to('cpu').numpy()) 可以看看他长啥样
+            all_correct_num += np.sum(current_correct_num.to('cpu').numpy(), axis=-1)
+            all_sample_num += current_correct_num.shape[0]
+        acc = all_correct_num / all_sample_num
+        total_acc.append(current_epoch)
+        total_acc.append(acc)
+        print('第{}轮完成,准确率: {:.3f}'.format(current_epoch,acc), flush=True)
+        if not os.path.isdir("models"):
+            os.mkdir("models")
+        torch.save(model, 'models/mnist_{:.3f}.pth'.format(acc))
+        # if np.abs(acc - prev_acc) < 1e-4:
+        #     plot(total_acc=total_acc)
+        #     print("训练完成！")
+        #     break
+        prev_acc = acc
+        if(current_epoch==50):
+            plot(total_acc=total_acc)
+    print("训练完成！")
